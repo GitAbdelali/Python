cfe5f6fad0678fa8a840c2c3e52a9db5e99442c5
Eljaouhari, Abdelali <eljaouhariabdelali.i@gmail.com>
2018-06-11 17:12:00 -0600

    Add IntFloatprint.py

---
+++ IntFloatPrint.py
@@ -0,0 +1,29 @@
+
+#   Writing things for each SentDex tutorial I've undergone: 
+#   Int functions only with integers (Whole number) ways to bypass this is by using Float
+#   Example: Print(Int(8+3.3)) will print only 11 
+#            However, print(float(8+3.3)) will print 11.3
+#   Text inside of single or double quotes makes the content inside it a string
+#   If an apostrophe is needed in a string, adding a backslash before it will not cause any problems
+#   Example: Print('We\'re going to have a great time learning python.)
+#   Concatenating brings things, (strings, links, folders, etc) together in a chain or series
+#   
+# 
+# 
+# 
+# 
+#   Mathematical expressions
+#   +, -, *, /, ** (to the power of)  
+
+# print('we're going to have some fun learning how to code) This will not work as the ' makes things weird
+# vs
+print("we're going to have some fun learning to code") 
+# or 
+print('we\'re going to have some fun learning to code')
+
+print(int(8+3.3))
+# Vs 
+print(float(8+3.3))
+# Other examples
+print(int(8*9))
+print(int(9/3))
