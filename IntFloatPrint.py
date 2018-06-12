d0f09aa064402c91a656d2858f3d1e44f27fc1b9
Eljaouhari, Abdelali <eljaouhariabdelali.i@gmail.com>
2018-06-12 03:37:29 -0600

    Done

--- IntFloatPrint.py
+++ IntFloatPrint.py
@@ -1,10 +1,8 @@
-
-#   Writing things for each SentDex tutorial I've undergone: 
 #   Int functions only with integers (Whole number) ways to bypass this is by using Float
 #   Example: Print(Int(8+3.3)) will print only 11 
 #            However, print(float(8+3.3)) will print 11.3
 #   Text inside of single or double quotes makes the content inside it a string
-#   If an apostrophe is needed in a string, adding a backslash before it will not cause any problems
+#   If an apostrophe is needed in a string, adding a backslash before it. This will not cause any problems
 #   Example: Print('We\'re going to have a great time learning python.)
 #   Concatenating brings things, (strings, links, folders, etc) together in a chain or series
 #   
