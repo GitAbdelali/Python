95596698fc7736a92108b963f5938c8e6aacd9e6
Eljaouhari, Abdelali <eljaouhariabdelali.i@gmail.com>
2018-06-16 01:12:17 -0600

    Sorted

---
+++ Homework/Average&Maximum.py
@@ -0,0 +1,20 @@
+# Write a Python program that reads in 3 numbers and displays the following:
+# a) the average of the three numbers
+# b) the maximum fo the three numbers
+
+
+print("Please enter three numbers of your choosing.")
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+num3 = float(input("Enter your third number: "))
+# Asks user to input three numbers individually
+if (num1 > num2) and (num1 > num3):
+      maximum = num1
+elif (num2 > num1) and (num2 > num3):
+      maximum = num2
+else: maximum = num3
+# Above is the code for the maximum
+average = ((num1 + num2 + num3)/3)
+print ("The average of the three numbers is:", average)
+print ("The Maximum of the numbers is:", maximum)
+# Above is the code for the average
---
+++ Learning/ForLoop.py
@@ -0,0 +1,24 @@
+# Similar to while loop, completely interchangeable
+
+# Comes down to preference. For loop is much more 
+# condensable compared to the while loop
+# For Loop is primarly used to iterate (Go) through a list
+
+# Some examples:
+
+exampleList = [4,5,6,7,8,9,10]
+
+# x can also be eachNumber
+# assigning a variable in the list above 
+
+# Iterater through a list
+for x in exampleList: 
+  print(x)
+  print('Continue Program')
+
+# For Loop as a counter
+# built in function range will output all the numbers between x,xx without listing xx
+for x in range(1,11):
+  print(x)
+
+  
